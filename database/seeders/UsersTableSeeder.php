36c76aea02e14e61fd59763df92503cdc03819e1
Lana Wong <lana.wong@gem.live>
2021-01-18 00:44:26 +0800

    added authentication

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -14,11 +14,11 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::create(['name' => 'Xin Hu','email' => 'xinhu@gmail.com','password' => 'IO67']);
-        User::create(['name' => 'Jon Ward','email' => 'jonward@gmail.com','password' => 'JT99']);
-        User::create(['name' => 'Cepren Macnob','email' => 'creprenmacnob@gmail.com','password' => 'TF37']);
-        User::create(['name' => 'Sjon Kilbourn','email' => 'sjonkilbourn@gmail.com','password' => 'GE63']);
-        User::create(['name' => 'Sticker ProMan','email' => 'stickerproman@gmail.com','password' => 'PO74']);
-        User::create(['name' => 'Whatsappvideo status','email' => 'whatsappvideostatus@gmail.com','password' => 'CM27']);
+        User::create(['name' => 'Xin Hu','email' => 'xinhu@gmail.com','password' => bcrypt('GQ71')]);
+        User::create(['name' => 'Jon Ward','email' => 'jonward@gmail.com','password' => bcrypt('AE86')]);
+        User::create(['name' => 'Cepren Macnob','email' => 'creprenmacnob@gmail.com','password' => bcrypt('UT39')]);
+        User::create(['name' => 'Sjon Kilbourn','email' => 'sjonkilbourn@gmail.com','password' => bcrypt('NK42')]);
+        User::create(['name' => 'Sticker ProMan','email' => 'stickerproman@gmail.com','password' => bcrypt('FB29')]);
+        User::create(['name' => 'Whatsappvideo status','email' => 'whatsappvideostatus@gmail.com','password' => bcrypt('MG22')]);
     }
 }
